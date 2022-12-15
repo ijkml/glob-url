9d651e59d0c27292d37ffe0bccb7c326e3a65e86
ML <me.mlaure@gmail.com>
2022-12-15 21:16:24 +0100

    chore: rename interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { matchRoute } from './matcher';
 
-export interface MiniMatch {
+export interface GlobUrl {
   match(pattern: string, route: string): boolean
   test(pattern: string, route: string): boolean
 }
 
-const matcher: MiniMatch = {
+const matcher: GlobUrl = {
   match: matchRoute,
   test: matchRoute,
 };
